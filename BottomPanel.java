7e2e8aa8dfc8af3681f3896ab5c5884a49aeffaf
Delvison Castillo <DelvisonCastillo@gmail.com>
2012-10-30 16:51:41 -0400

    trying to fix layout

--- BottomPanel.java
+++ BottomPanel.java
@@ -23,30 +23,45 @@ class BottomPanel extends JPanel {
     this.setLayout(gbag); //layout for panel
 
     leftMenu = new JList(menuOptions);
+    JPanel menuTop = new JPanel();
+    menuTop.setPreferredSize(new Dimension(30,30));
+    menuTop.add(new JLabel("Menu"));
+    JPanel menuPane = new JPanel();
+    //menuPane.setLayout(new GridLayout(0,1));
+    //menuPane.setLayout(new GridLayout(0,1));
+    menuPane.setLayout(new BoxLayout(menuPane, BoxLayout.Y_AXIS)); 
+    menuPane.add(leftMenu);
+    //menuPane.add(menuTop);
+    menuPane.setBorder(BorderFactory.createEtchedBorder());
+
     playlist = new JList(menuOptions);
     library = new JTable(sampleTracks,tableHeaders);
     JScrollPane lib = new JScrollPane(library);
     JScrollPane pList = new JScrollPane(playlist);
     JScrollPane scroll = new JScrollPane(library);
-    libraryAndPlaylist = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, scroll, pList);
 
-    leftMenu.setPreferredSize(new Dimension(100,410));
+    //leftMenu.setPreferredSize(new Dimension(100,410));
+    JSplitPane b = new JSplitPane(JSplitPane.VERTICAL_SPLIT,scroll,pList);
+    JPanel top = new JPanel();
+    top.add(b);
+    libraryAndPlaylist = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, menuPane, top);
+
     //leftMenu.setMaximumSize(new Dimension(1000,1000));
 
     //leftMenu.addMouseListener(mouseListener); //add a mouse listener
     //playList.addMouseListener(mouseListener2); //add a mouse listener
     gbc.gridx =0;
     gbc.gridy =0;
-    gbag.setConstraints(leftMenu, gbc);
-    leftMenu.setBorder(BorderFactory.createEtchedBorder());
-    gbc.anchor = GridBagConstraints.WEST;
-    this.add(leftMenu);
-
-    gbc.gridx =1;
-    gbc.gridy =0;
+    //gbag.setConstraints(leftMenu, gbc);
     gbag.setConstraints(libraryAndPlaylist, gbc);
-
+    gbc.anchor = GridBagConstraints.WEST;
     this.add(libraryAndPlaylist);
+
+  //  gbc.gridx =1;
+  //  gbc.gridy =0;
+ //   gbag.setConstraints(libraryAndPlaylist, gbc);
+
+   // this.add(libraryAndPlaylist);
   }
 
   /*
